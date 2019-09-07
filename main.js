7cf22ea49972d8e110316c998d9322ff50434864
Nitin Jayant <thenitinjayant@gmail.com>
2019-09-07 23:52:10 +0530

    responsive

--- main.js
+++ main.js
@@ -5,3 +5,21 @@ $(document).ready(function () {
             .slideToggle();
     });
 });
+
+
+// Smooth Scrolling
+
+$('.nav-link').on('click', function(event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+  
+      const hash = this.hash;
+  
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top 
+        },
+        800
+      );
+    }
+  });
